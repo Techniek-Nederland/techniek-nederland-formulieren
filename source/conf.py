47a3e5562cea6bd6c253c7338a867a1c0daa8e34
Wouter Klein Heerenbrink <wouter@fluxility.com>
2023-06-07 10:31:54 +0200

    Remove some of details in access management and refer more to docs

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,7 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autosectionlabel",
     'sphinxcontrib.httpdomain',
     'sphinxcontrib.httpexample',
 ]
@@ -90,3 +91,5 @@ html_logo = "_static/logo.svg"
 html_favicon = "_static/favicon.ico"
 
 httpexample_scheme = 'https'
+
+autosectionlabel_prefix_document = True
